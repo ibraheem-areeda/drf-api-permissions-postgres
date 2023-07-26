40182a5080584f85844718380938f758984dc768
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-07-26 21:40:59 +0000

    p2

--- django_items/settings.py
+++ django_items/settings.py
@@ -81,9 +81,14 @@ WSGI_APPLICATION = "django_items.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+
     }
 }
 
